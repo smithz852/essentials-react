0efa4edece4673b0ef0c18f3c334cfc74e81858f
Zach Smith <zachsmith852@gmail.com>
2024-09-12 19:21:12 -0700

    Add basic function to recognize input text in boxes

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import InputBox from "./InputBox";
 
 export default function() {
+const [investValue, setInvestValue] = useState()
+
+function handleInputChange(valId, newNumber) {
+  setInvestValue(prevValue => {
+    return {
+      ...prevValue,
+      [valId]: newNumber
+    }
+  })
+}
+
   return (
     <>
       <div id="user-input"  >
         
-          <InputBox name="Initial Investment"/>
-          <InputBox name="Annual Investment"/>
-          <InputBox name="Expected Return"/>
-          <InputBox name="Duration"/>
+          <InputBox name="Initial Investment" valId="1" onChangeValue={handleInputChange}/>
+          <InputBox name="Annual Investment" valId="2" onChangeValue={handleInputChange}/>
+          <InputBox name="Expected Return" valId="3" onChangeValue={handleInputChange}/>
+          <InputBox name="Duration" valId="4" onChangeValue={handleInputChange}/>
         
       </div>
     </>
