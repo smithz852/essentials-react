9948dcb22ad9dbbc3bac46c89522640896fb193b
Zach Smith <zachsmith852@gmail.com>
2024-09-12 20:45:02 -0700

    Lift state to main app to use data in tables

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import DataContainer from "./components/DataContainer"
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import { useState } from "react"
+
+
+
 
 function App() {
+  const [investValue, setInvestValue] = useState()
+
+function handleInputChange(valId, newNumber) {
+  setInvestValue(prevValue => {
+    console.log(valId)
+    return {
+      ...prevValue,
+      [valId]: newNumber
+    }
+  })
+}
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChangeValue={handleInputChange} />
       <DataContainer />
     </>
   )
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,26 +1,21 @@
 import { useState } from "react";
 import InputBox from "./InputBox";
 
-export default function() {
-const [investValue, setInvestValue] = useState()
+export default function({ onChangeValue }) {
 
-function handleInputChange(valId, newNumber) {
-  setInvestValue(prevValue => {
-    return {
-      ...prevValue,
-      [valId]: newNumber
-    }
-  })
-}
+
+  function secondChange(valId, newNumber) {
+    onChangeValue(valId, newNumber)
+  }
 
   return (
     <>
       <div id="user-input"  >
         
-          <InputBox name="Initial Investment" valId="1" onChangeValue={handleInputChange}/>
-          <InputBox name="Annual Investment" valId="2" onChangeValue={handleInputChange}/>
-          <InputBox name="Expected Return" valId="3" onChangeValue={handleInputChange}/>
-          <InputBox name="Duration" valId="4" onChangeValue={handleInputChange}/>
+          <InputBox name="Initial Investment" valId="1" theValue={secondChange}/>
+          <InputBox name="Annual Investment" valId="2" theValue={secondChange}/>
+          <InputBox name="Expected Return" valId="3" theValue={secondChange}/>
+          <InputBox name="Duration" valId="4" theValue={secondChange}/>
         
       </div>
     </>
