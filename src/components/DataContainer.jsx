55d0ac06fb12f5ba89a275ca1e391bc0651eb728
Zach Smith <zachsmith852@gmail.com>
2024-09-19 21:27:42 -0700

    generate financial returns from investment function

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,7 +1,46 @@
-
+import { calculateInvestmentResults } from '../util/investment.js'
 
 export default function DataContainer({ data }) {
+
   console.log('data', data)
+
+  let initialInvestment = parseInt(data.initialInvestment)
+  let annualInvestment = parseInt(data.annualInvestment)
+  let expectedReturn = parseInt(data.expectedReturn)
+  let duration = parseInt(data.duration)
+
+  if (!initialInvestment) {
+    initialInvestment = 0
+  }
+
+  if (!annualInvestment) {
+    annualInvestment = 0
+  }
+
+  if (!expectedReturn) {
+    expectedReturn = 0
+  }
+
+  if (!duration) {
+    duration = 0
+  }
+
+  const collectiveData = {
+    initialInvestment,
+    annualInvestment,
+    expectedReturn,
+    duration
+  }
+
+  console.log(initialInvestment)
+  console.log(annualInvestment)
+  console.log(expectedReturn)
+  console.log(duration)
+
+
+  const dataResults = calculateInvestmentResults(collectiveData)
+  console.log('results', dataResults)
+
   return (
     <>
       <table id="result" >
