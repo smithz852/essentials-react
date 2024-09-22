1de5e856778ad09e56938e5c4c3dc522c94a113f
Zach Smith <zachsmith852@gmail.com>
2024-09-21 21:02:29 -0700

    adjustment to deploy site

---
+++ src/components/DataColumn.jsx
@@ -0,0 +1,19 @@
+export default function DataColumn({ title }) {
+  return (
+   <table>
+    <thead>
+      <tr>
+        <th>{title}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>$45</td>
+      </tr>
+      <tr>
+        <td>$45</td>
+      </tr>
+    </tbody>
+   </table>
+  )
+}
